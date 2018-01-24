4ef5ec044a2ca3631eeb494359b8ea667c581a6a
coma-meth <coma.meth@gmail.com>
2018-01-24 17:02:05 +0100

    add code

---
+++ weights_class.py
@@ -0,0 +1,102 @@
+import os
+import os.path as op
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pypet
+from collections import OrderedDict
+from sklearn.model_selection import (StratifiedKFold, StratifiedShuffleSplit,
+                                     cross_val_score)
+from pypet.features import compute_regional_features
+from sklearn.svm import SVC
+from sklearn.dummy import DummyClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler, RobustScaler
+from sklearn.metrics import (roc_auc_score, precision_recall_curve,
+                             classification_report, precision_score,
+                             recall_score)
+from sklearn.feature_selection import SelectKBest, f_classif, SelectPercentile
+
+if os.uname()[1] == 'antogeo-XPS':
+    db_path = '/home/antogeo/data/PET/pet_suv_db/'
+elif os.uname()[1] == 'comameth':
+    db_path = '/home/coma_meth/Documents/PET/pet_suv_db/'
+elif os.uname()[1] in ['mia.local', 'mia']:
+    db_path = '/Users/fraimondo/data/pet_suv_db/'
+
+meta_fname = op.join(db_path, 'extra', 'SUV_database10172017.xlsx')
+df = compute_regional_features(db_path, meta_fname)
+df = df.query('QC_PASS == True and ML_VALIDATION == False')
+
+weight_val = np.arange(1, 10, .2)
+weight_val = sorted(np.concatenate((1 / weight_val, weight_val)))
+
+classifiers = OrderedDict()
+
+markers = [x for x in df.columns if 'aal' in x]
+X = df[markers].values
+y = 1 - (df['Final diagnosis (behav)'] == 'VS').values.astype(np.int)
+
+results = dict()
+results['Iteration'] = []
+results['Weight Val'] = []
+results['Classifier'] = []
+results['AUC'] = []
+results['Precision'] = []
+results['Recall'] = []
+
+sss = StratifiedShuffleSplit(
+    n_splits=50, test_size=0.3, random_state=42)
+
+for t_iter, (train, test) in enumerate(sss.split(X, y)):
+    for val in weight_val:
+        classifiers['SVC_fs_W40_10'] = Pipeline([
+                ('scaler', RobustScaler()),
+                ('select', SelectPercentile(f_classif, 10.)),
+                ('clf', SVC(kernel="linear", C=1,  probability=True,
+                            class_weight={0: .25, 1: 1}))
+            ])
+        classifiers['SVC_fs_W10_26'] = Pipeline([
+                ('scaler', RobustScaler()),
+                ('select', SelectPercentile(f_classif, 10.)),
+                ('clf', SVC(kernel="linear", C=1,  probability=True,
+                            class_weight={0: 1, 1: 2.6}))
+            ])
+        classifiers['Dummy'] = Pipeline([
+                ('clf', DummyClassifier(strategy="most_frequent",random_state=42))
+            ])
+        print('Iteration {}'.format(t_iter))
+        X_train, X_test = X[train], X[test]
+        y_train, y_test = y[train], y[test]
+        for clf_name, clf in classifiers.items():
+            # Fit the model on the training set
+            clf.fit(X_train, y_train)
+
+            # Predict the test set
+            y_pred_proba = clf.predict_proba(X_test)[:, 1]
+            y_pred_class = clf.predict(X_test)
+
+            auc_score = roc_auc_score(y_test, y_pred_proba)
+            prec_score = precision_score(y_test, y_pred_class)
+            rec_score = recall_score(y_test, y_pred_class)
+
+            results['Weight Val'].append(val)
+            results['Iteration'].append(t_iter)
+            results['Classifier'].append(clf_name)
+            results['AUC'].append(auc_score)
+            results['Precision'].append(prec_score)
+            results['Recall'].append(rec_score)
+
+df = pd.DataFrame(results)
+df.to_csv(
+    '/home/coma_meth/Documents/antogeo/git_codes/PET_class/scratch/weights_eval.csv')
+cw_mcs = np.sum(y)/y.shape[0]
+float(np.sum(y)) / y.shape[0]
+float(y.shape[0] - np.sum(y)) / y.shape[0]
+
+sns.pointplot(x="Weight Val", y="Recall", hue="Classifier", data=df)
+fig, ax = plt.subplots(1,1)
+sns.pointplot(x="Weight Val", y="Recall", hue="Classifier", data=df, ax=ax)
+sns.pointplot(x="Weight Val", y="Precision", hue="Classifier", data=df, ax=ax)
+sns.pointplot(x="Weight Val", y="AUC", hue="Classifier", data=df, ax=ax)
