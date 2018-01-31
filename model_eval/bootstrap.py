96174d13c8ed03807b99386862b2ed256f95b1fa
coma-meth <coma.meth@gmail.com>
2018-01-31 15:44:11 +0100

    debug bootstrapping

--- model_eval/bootstrap.py
+++ model_eval/bootstrap.py
@@ -101,8 +101,46 @@ df = pd.DataFrame(results)
 df.to_csv('models_eval.csv')
 
 # plot scores
-pyplot.hist(results)
-pyplot.show()
+import os
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+df = pd.read_csv('boot_1000.csv')
+
+SVM_AUC = df.loc[df['Classifier'] == 'SVC_fs_W40_10', 'AUC']
+plt.hist(SVM_AUC, histtype='stepfilled', align='mid')
+SVM_rec = df.loc[df['Classifier'] == 'SVC_fs_W40_10', 'Recall']
+plt.hist(SVM_rec, histtype='stepfilled', align='mid')
+SVM_prec = df.loc[df['Classifier'] == 'SVC_fs_W40_10', 'Precision']
+plt.hist(SVM_prec, histtype='stepfilled', align='mid')
+plt.show()
+
+SVM2_AUC = df.loc[df['Classifier'] == 'SVC_fs_W10_26', 'AUC']
+plt.hist(SVM2_AUC, histtype='stepfilled', align='mid')
+SVM2_rec = df.loc[df['Classifier'] == 'SVC_fs_W10_26', 'Recall']
+plt.hist(SVM2_rec, histtype='stepfilled', align='mid')
+SVM2_prec = df.loc[df['Classifier'] == 'SVC_fs_W10_26', 'Precision']
+plt.hist(SVM2_prec, histtype='stepfilled', align='mid')
+plt.show()
+
+RF_AUC = df.loc[df['Classifier'] == 'RF_w', 'AUC']
+plt.hist(RF_AUC, histtype='stepfilled', align='mid')
+RF_rec = df.loc[df['Classifier'] == 'RF_w', 'Recall']
+plt.hist(RF_rec, histtype='stepfilled', align='mid')
+RF_prec = df.loc[df['Classifier'] == 'RF_w', 'Precision']
+plt.hist(RF_prec, histtype='stepfilled', align='mid')
+plt.show()
+
+Dummy_AUC = df.loc[df['Classifier'] == 'Dummy', 'AUC']
+plt.hist(Dummy_AUC, histtype='stepfilled', align='mid', color='blue')
+Dummy_rec = df.loc[df['Classifier'] == 'Dummy', 'Recall']
+plt.hist(Dummy_rec, histtype='stepfilled', align='mid', color='green')
+Dummy_prec = df.loc[df['Classifier'] == 'Dummy', 'Precision']
+plt.hist(Dummy_prec, histtype='barstacked', align='mid', color='red')
+plt.show()
+
 # confidence intervals
 alpha = 0.95
 p = ((1.0-alpha)/2.0) * 100
