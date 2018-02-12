e03954e43499bebe65f78d34978e0326e7a9aea3
antogeo <antogeo@gmail.com>
2018-02-12 15:14:51 +0100

    debug

--- model_eval/read_plot_res.py
+++ model_eval/read_plot_res.py
@@ -6,8 +6,11 @@ import matplotlib.pyplot as plt
 df = pd.read_csv('models_eval.csv')
 
 fig, axes = plt.subplots(1, 3, figsize=(12, 6))
-sns.swarmplot(x="Classifier", y="Recall", hue="Classifier", data=df, ax=axes[0])
-sns.swarmplot(x="Classifier", y="Precision", hue="Classifier", data=df, ax=axes[1])
-sns.swarmplot(x="Classifier", y="AUC", hue="Classifier", data=df, ax=axes[2])
+sns.swarmplot(
+    x="Classifier", y="Recall", hue="Classifier", data=df, ax=axes[0])
+sns.swarmplot(
+    x="Classifier", y="Precision", hue="Classifier", data=df, ax=axes[1])
+sns.swarmplot(
+    x="Classifier", y="AUC", hue="Classifier", data=df, ax=axes[2])
 plt.savefig('figures/model_compar.pdf')
 plt.show()
--- validation/read_plot_res.py
+++ validation/read_plot_res.py
@@ -3,18 +3,24 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('boot_1000.csv')
+df = pd.read_csv('validation/boot_1000.csv')
 
 fig, axes = plt.subplots(1, 3, figsize=(12, 6))
-sns.swarmplot(x="Classifier", y="Recall", hue="Classifier", data=df, ax=axes[0])
-sns.swarmplot(x="Classifier", y="Precision", hue="Classifier", data=df, ax=axes[1])
-sns.swarmplot(x="Classifier", y="AUC", hue="Classifier", data=df, ax=axes[2])
-plt.savefig('figures/model_compar.pdf')
+sns.swarmplot(
+    x="Classifier", y="Recall", hue="Classifier", data=df, ax=axes[0])
+sns.swarmplot(
+    x="Classifier", y="Precision", hue="Classifier", data=df, ax=axes[1])
+sns.swarmplot(
+    x="Classifier", y="AUC", hue="Classifier", data=df, ax=axes[2])
+# plt.savefig('figures/model_compar.pdf')
 plt.show()
 
 fig, axes = plt.subplots(1, 3, figsize=(12, 6))
-sns.boxplot(x="Classifier", y="Recall", hue="Classifier", data=df, ax=axes[0])
-sns.boxplot(x="Classifier", y="Precision", hue="Classifier", data=df, ax=axes[1])
-sns.boxplot(x="Classifier", y="AUC", hue="Classifier", data=df, ax=axes[2])
-plt.savefig('figures/model_compar.pdf')
+sns.boxplot(
+    x="Classifier", y="Recall", hue="Classifier", data=df, ax=axes[0])
+sns.boxplot(
+    x="Classifier", y="Precision", hue="Classifier", data=df, ax=axes[1])
+sns.boxplot(
+    x="Classifier", y="AUC", hue="Classifier", data=df, ax=axes[2])
+# plt.savefig('/model_comparbox.pdf')
 plt.show()
