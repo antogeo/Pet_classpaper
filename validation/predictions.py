c09c41b5c898026d1da33fafc71e9a794684bbf8
coma-meth <coma.meth@gmail.com>
2018-02-15 18:42:40 +0100

    debug double dip in compute_bootstrap

--- validation/predictions.py
+++ validation/predictions.py
@@ -57,68 +57,20 @@ markers = [x for x in df.columns if 'aal' in x]
 X_train = df_train[markers].values
 y_train = 1 - (df_train['Final diagnosis (behav)'] == 'VS').values.astype(np.int)
 
-# configure bootstrap
-t_iter = 1000
-
 y_val = 1 - (df_val['Final diagnosis (behav)'] == 'VS').values.astype(np.int)
 sizes = [np.sum(y_val == 0), np.sum(y_val == 1)]
 
 # run bootstrap
 
 results = dict()
-results['Iteration'] = []
-results['Classifier'] = []
-results['AUC'] = []
-results['Precision'] = []
-results['Recall'] = []
-
-predictions = dict()
-predictions['Patient'] = []
-predictions['Classifier'] = []
-predictions['Label'] = []
-
-
+results['Patient'] = []
+results['SVC_fs_W40_10'] = []
+results['SVC_fs_W10_26'] = []
+results['RF_w'] = []
+results['Dummy'] = []
 
 for clf_name, clf in classifiers.items():
     # Fit the model on the training set\
     print(clf_name, clf)
     clf.fit(X_train, y_train)
-    probas = clf.predict_proba(X_train)[:, 1]
-    auc_score = roc_auc_score(y_train, probas)
-    print('In sample AUC: {}'.format(auc_score))
-
-
-for i in range(t_iter):
-    # prepare train and test sets
-    df_test_vs = resample(
-        df_val[y_val == 0], n_samples=sizes[0], random_state=i)
-    df_test_mcs = resample(
-        df_val[y_val == 1], n_samples=sizes[1], random_state=i)
-
-    df_test = df_test_vs.append(df_test_mcs)
-
-    X_test = df_test[markers].values
-    y_test = 1 - (
-        df_test['Final diagnosis (behav)'] == 'VS').values.astype(np.int)
-    # fit model
-
-    for clf_name, clf in classifiers.items():
-        # Predict the test set
-        y_pred_proba = clf.predict_proba(X_test)[:, 1]
-        y_pred_class = clf.predict(X_test)
-
-        auc_score = roc_auc_score(y_test, y_pred_proba)
-        prec_score = precision_score(y_test, y_pred_class)
-        rec_score = recall_score(y_test, y_pred_class)
-
-        results['Iteration'].append(t_iter)
-        results['Classifier'].append(clf_name)
-        results['AUC'].append(auc_score)
-        results['Precision'].append(prec_score)
-        results['Recall'].append(rec_score)
-        print('Iter {} Clf = {} AUC = {} Prec = {} Rec = {}'.format(
-            i, clf_name, auc_score, prec_score, rec_score))
-
-
-df = pd.DataFrame(results)
-df.to_csv('boot_1000.csv')
+    clf.predict(X_test)
