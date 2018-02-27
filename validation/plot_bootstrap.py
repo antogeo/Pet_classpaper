175aefeac15e960f27bb6496d9c72b95a74f96a0
coma-meth <coma.meth@gmail.com>
2018-02-27 17:23:28 +0100

    add conf intervals

--- validation/plot_bootstrap.py
+++ validation/plot_bootstrap.py
@@ -13,40 +13,43 @@ fig_mean, axes = pypet.viz.plot_values(
     classes=['SVC_fs_W40_10', 'SVC_fs_W10_26', 'RF_w', 'Dummy'])
 plt.show()
 
+SVM_AUC = df.loc[df['Classifier'] == 'SVC_fs_W40_10', 'AUC']
+SVM_rec = df.loc[df['Classifier'] == 'SVC_fs_W40_10', 'Recall']
+SVM_prec = df.loc[df['Classifier'] == 'SVC_fs_W40_10', 'Precision']
+SVM2_AUC = df.loc[df['Classifier'] == 'SVC_fs_W10_26', 'AUC']
+SVM2_rec = df.loc[df['Classifier'] == 'SVC_fs_W10_26', 'Recall']
+SVM2_prec = df.loc[df['Classifier'] == 'SVC_fs_W10_26', 'Precision']
+RF_AUC = df.loc[df['Classifier'] == 'RF_w', 'AUC']
+RF_rec = df.loc[df['Classifier'] == 'RF_w', 'Recall']
+RF_prec = df.loc[df['Classifier'] == 'RF_w', 'Precision']
+Dummy_AUC = df.loc[df['Classifier'] == 'Dummy', 'AUC']
+Dummy_rec = df.loc[df['Classifier'] == 'Dummy', 'Recall']
+Dummy_prec = df.loc[df['Classifier'] == 'Dummy', 'Precision']
+
 plt.figure(1)
 plt.subplot(221)
-SVM_AUC = df.loc[df['Classifier'] == 'SVC_fs_W40_10', 'AUC']
 plt.hist(SVM_AUC, histtype='stepfilled', align='mid', color='blue', bins=50)
-SVM_rec = df.loc[df['Classifier'] == 'SVC_fs_W40_10', 'Recall']
 plt.hist(SVM_rec, histtype='stepfilled', align='mid', color='green', bins=50)
-SVM_prec = df.loc[df['Classifier'] == 'SVC_fs_W40_10', 'Precision']
 plt.hist(SVM_prec, histtype='stepfilled', align='mid', color='red', bins=50)
 plt.show()
 
+
 plt.subplot(222)
-SVM2_AUC = df.loc[df['Classifier'] == 'SVC_fs_W10_26', 'AUC']
 plt.hist(SVM2_AUC, histtype='stepfilled', align='mid', color='blue', bins=50)
-SVM2_rec = df.loc[df['Classifier'] == 'SVC_fs_W10_26', 'Recall']
 plt.hist(SVM2_rec, histtype='stepfilled', align='mid', color='green', bins=50)
-SVM2_prec = df.loc[df['Classifier'] == 'SVC_fs_W10_26', 'Precision']
 plt.hist(SVM2_prec, histtype='stepfilled', align='mid', color='red', bins=50)
 plt.show()
 
+
 plt.subplot(223)
-RF_AUC = df.loc[df['Classifier'] == 'RF_w', 'AUC']
 plt.hist(RF_AUC, histtype='stepfilled', align='mid', color='blue', bins=50)
-RF_rec = df.loc[df['Classifier'] == 'RF_w', 'Recall']
 plt.hist(RF_rec, histtype='stepfilled', align='mid', color='green', bins=50)
-RF_prec = df.loc[df['Classifier'] == 'RF_w', 'Precision']
 plt.hist(RF_prec, histtype='stepfilled', align='mid', color='red', bins=50)
 plt.show()
 
 plt.subplot(224)
-Dummy_AUC = df.loc[df['Classifier'] == 'Dummy', 'AUC']
 plt.hist(Dummy_AUC, histtype='stepfilled', align='mid', color='blue', bins=50)
-Dummy_rec = df.loc[df['Classifier'] == 'Dummy', 'Recall']
 plt.hist(Dummy_rec, histtype='stepfilled', align='mid', color='green', bins=50)
-Dummy_prec = df.loc[df['Classifier'] == 'Dummy', 'Precision']
 plt.hist(Dummy_prec, histtype='barstacked', align='mid', color='red', bins=50)
 plt.show()
 
@@ -63,50 +66,34 @@ for clf in np.unique(df['Classifier']):
         df.loc[df['Classifier'] == clf, 'Recall'].mean())
     results['Mean Precision'].append(
         df.loc[df['Classifier'] == clf, 'Precision'].mean())
+    alpha = 0.95
+    p_up = ((1.0-alpha)/2.0) * 100
+    p_low = (alpha+((1.0-alpha)/2.0)) * 100
 
+    lower_AUC = max(0.0, np.percentile(
+        df.loc[df['Classifier'] == clf, 'AUC'], p_up))
+    upper_AUC = min(1.0, np.percentile(
+        df.loc[df['Classifier'] == clf, 'AUC'], p_low))
+    print('%.1f confidence interval of AUC for %s:  %.1f%% and %.1f%%' % (
+        alpha*100, clf, lower_AUC*100, upper_AUC*100))
 
-Dummy_AUC_mean = Dummy_AUC.mean()
-Dummy_rec_mean = Dummy_rec.mean()
-Dummy_prec_mean = Dummy_prec.mean()
+    lower_Rec = max(0.0, np.percentile(
+        df.loc[df['Classifier'] == clf, 'Recall'], p_up))
+    upper_Rec = min(1.0, np.percentile(
+        df.loc[df['Classifier'] == clf, 'Recall'], p_low))
+    print('%.1f confidence interval of Recall for %s:  %.1f%% and %.1f%%' % (
+        alpha*100, clf, lower_Rec*100, upper_Rec*100))
 
-RF_AUC_mean = RF_AUC.mean()
-RF_rec_mean = RF_rec.mean()
-RF_prec_mean = RF_prec.mean()
+    lower_Prec = max(0.0, np.percentile(
+        df.loc[df['Classifier'] == clf, 'Precision'], p_up))
+    upper_Prec = min(1.0, np.percentile(
+        df.loc[df['Classifier'] == clf, 'Precision'], p_low))
+    print('%.1f confidence interval of Precision for %s:  %.1f%% and %.1f%%' % (
+        alpha*100, clf, lower_Prec*100, upper_Prec*100))
 
-SVM_AUC_mean = SVM_AUC.mean()
-SVM_rec_mean = SVM_rec.mean()
-SVM_prec_mean = SVM_prec.mean()
-
-SVM2_AUC_mean = SVM2_AUC.mean()
-SVM2_rec_mean = SVM2_rec.mean()
-SVM2_prec_mean = SVM2_prec.mean()
-
-
-# confidence intervals
-alpha = 0.95
-p = ((1.0-alpha)/2.0) * 100
-lower = max(0.0, np.percentile(RF_AUC, p))
-p = (alpha+((1.0-alpha)/2.0)) * 100
-upper = min(1.0, np.percentile(RF_AUC, p))
-print('%.1f confidence interval of AUC:  %.1f%% and %.1f%%' % (
-    alpha*100, lower*100, upper*100))
 print('AUC mean of Dummy:  %.1f%%' % (
-    Dummy_AUC_mean))
-
-p = ((1.0-alpha)/2.0) * 100
-lower = max(0.0, np.percentile(RF_rec, p))
-p = (alpha+((1.0-alpha)/2.0)) * 100
-upper = min(1.0, np.percentile(RF_rec, p))
-print('%.1f confidence interval of Recall %.1f%% and %.1f%%' % (
-    alpha*100, lower*100, upper*100))
+    results['Mean AUC']['Classifier'== 'Dummy']))
 print('Recall mean of Dummy:  %.1f%%' % (
-    Dummy_rec_mean))
-
-p = ((1.0-alpha)/2.0) * 100
-lower = max(0.0, np.percentile(RF_prec, p))
-p = (alpha+((1.0-alpha)/2.0)) * 100
-upper = min(1.0, np.percentile(RF_prec, p))
-print('%.1f confidence intervalof Precision %.1f%% and %.1f%%' % (
-    alpha*100, lower*100, upper*100))
+    results['Mean Recall']['Classifier'== 'Dummy']))
 print('Prec mean of Dummy:  %.1f%%' % (
-    Dummy_prec_mean))
+     results['Mean Precision']['Classifier'== 'Dummy']))
