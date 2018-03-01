dad193943116b492fbc8e91312862c9a39d1102b
antogeo <antogeo@gmail.com>
2018-03-01 02:08:50 +0100

    plot predictions

--- validation/plot_predict.py
+++ validation/plot_predict.py
@@ -4,17 +4,30 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pypet
 
-df = pd.read_csv('predictions.csv')
-color_lbl = df.loc[df['Classifier'] == 'RF_w', 'Label']
+df = pd.read_csv('validation/predictions.csv')
 
-plt.figure()
-plt.scatter(
-    df.loc[df['Classifier'] == 'RF_w', 'Probability'],
-    df.loc[df['Classifier'] == 'RF_w', 'Unnamed: 0'],
-    s=80, c=color_lbl, marker="v")
-plt.scatter(
-    df.loc[df['Classifier'] == 'RF_w', 'Probability'],
-    df.loc[df['Classifier'] == 'RF_w', 'Unnamed: 0'],
-    s=40, c='yellow', marker="+")
-plt.plot([.5, .5], [100, 160], 'k-', lw=1)
-plt.show()
+color = ['y' if x == 1 else 'b' for x in df.loc[
+    df['Classifier'] == 'RF_w', 'Label']]
+shape = ['o' if x == 1 else 's' for x in df.loc[
+    df['Classifier'] == 'SVC_fs_W10_26', 'Prediction']]
+fill = ['x' if x == 1 else '+' for x in df.loc[
+    df['Classifier'] == 'SVC_fs_W10_26', 'Prediction']]
+
+fig, ax = plt.subplots()
+for i, prob in enumerate(df.loc[df['Classifier'] == 'RF_w', 'Probability']):
+    ax.scatter(prob, i, s=120, c=color[i], marker=shape[i])
+
+for i, prob in enumerate(df.loc[df['Classifier'] == 'RF_w', 'Probability']):
+    ax.scatter(prob, i, s=120, c='r', marker=fill[i])
+ax.plot([.5, .5], [0, 50], 'k-', lw=1)
+
+ax.legend()
+ax.grid(True)
+fig.show()
+df.columns
+fig2, ax2 = plt.subplots()
+for ind, name in enumerate(np.unique(df['Code'])):
+    color = ['b' if df.loc[df['Code'] == name, 'Label'].mean() == 1 else 'y']
+    ax2.scatter(df.loc[df['Code'] == name, 'Probability'].mean(), ind,
+                s=120, c=color, marker='o')
+ax2.plot([.5, .5], [0, 50], 'k-', lw=1)
