f2297c243cc708c18159f4d031da943ee6715fc5
antogeo <antogeo@gmail.com>
2018-05-09 01:57:50 +0200

    feature eval plot

--- validation/read_plot_res.py
+++ validation/read_plot_res.py
@@ -3,7 +3,7 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('validation/boot_1000.csv')
+df = pd.read_csv('model_eval/boot_1000.csv')
 
 fig, axes = plt.subplots(1, 3, figsize=(12, 6))
 sns.swarmplot(
@@ -22,5 +22,5 @@ sns.boxplot(
     x="Classifier", y="Precision", hue="Classifier", data=df, ax=axes[1])
 sns.boxplot(
     x="Classifier", y="AUC", hue="Classifier", data=df, ax=axes[2])
-plt.savefig('figures/model_compar_box.pdf')
+# plt.savefig('figures/model_compar_box.pdf')
 plt.show()
