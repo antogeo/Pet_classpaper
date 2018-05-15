896cd5ef77d88d94b0e558fef0eaaf70ea224d39
antogeo <antogeo@gmail.com>
2018-05-15 03:32:22 +0200

    feature matrix

--- features/compute_feat_matrix.py
+++ features/compute_feat_matrix.py
@@ -2,36 +2,13 @@ import os
 import os.path as op
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 import pypet
 from collections import OrderedDict
-from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.model_selection import StratifiedKFold
 from pypet.features import compute_regional_features
 from sklearn.svm import SVC
-from sklearn.dummy import DummyClassifier
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import RobustScaler
-from sklearn.metrics import (roc_auc_score, precision_score, recall_score)
-from sklearn.feature_selection import f_classif, SelectPercentile
-import re
-
-
-def conv_text(c):
-    return int(c) if c.isdigit() else c
-
-
-def keyfinder(key):
-    return(conv_text(c) for c in re.split('([0-9]+)', key))
-
-
-def sorted_nicely(l):
-    # """ Sort the given iterable in the way that humans expect."""
-    # convert = lambda text: int(text) if text.isdigit() else text
-    # alphanum_key = lambda key: [
-    #     convert(c) for c in re.split('([0-9]+)', key)]
-    return sorted(l, key=keyfinder(l))
-
 
 if os.uname()[1] == 'antogeo-XPS':
     db_path = '/home/antogeo/data/PET/pet_suv_db/'
@@ -44,57 +21,41 @@ meta_fname = op.join(db_path, 'extra', 'SUV_database10172017_2.xlsx')
 df = compute_regional_features(db_path, meta_fname)
 df = df.query('QC_PASS == True and ML_VALIDATION == False')
 
-classifiers = OrderedDict()
 
-markers = [x for x in df.columns if 'aal' in x]
+fcols = [x for x in df.columns if 'aal' in x]
+markers = sorted(fcols, key=lambda x: int(x.replace('aal_atlas_', '')))
+
+X = df.query('QC_PASS == True and ML_VALIDATION == False')
+df = df.reset_index()
 X = df[markers].values
 y = 1 - (df['Final diagnosis (behav)'] == 'VS').values.astype(np.int)
 
-classifiers['SVC_fs_W40_10'] = Pipeline([
+# TODO: select classifiers
+
+classifiers = OrderedDict()
+classifiers['SVC'] = Pipeline([
         ('scaler', RobustScaler()),
-        ('clf', SVC(kernel="linear", C=1, probability=True,
-                    class_weight={0: 1, 1: .25}))
-    ])
-classifiers['SVC_fs_W10_26'] = Pipeline([
-        ('scaler', RobustScaler()),
-        ('clf', SVC(kernel="linear", C=1,  probability=True,
-                    class_weight={0: 1, 1: 2.6}))
+        ('clf', SVC(kernel="linear", C=1))
     ])
 
-results = dict()
-results['Iteration'] = []
-results['Classifier'] = []
-results['AUC'] = []
-results['Precision'] = []
-results['Recall'] = []
+iter = 2
+results = np.zeros((len(df), len(markers), iter))
 
-sss = StratifiedShuffleSplit(
-    n_splits=500, test_size=0.3, random_state=42)
-for x in df.columns:
-    if 'aal' in x:
-        print(df[x].values)
+for t_iter in range(iter):
+    skf = StratifiedKFold(n_splits=5, random_state=t_iter)
+    for train, test in skf.split(X, y):
+        for f_num, feature in enumerate(markers):
+            X_train, X_test = [X[train, f_num]], [X[test, f_num]]
+            y_train, y_test = y[train], y[test]
+            y_score = []
+            for clf_name, clf in classifiers.items():
+                # Fit the model on the training set
+                clf.fit(X_train, y_train)
+                y_pred_class = clf.predict(X_test)
+                y_score.append(y_pred_class)
+                # TODO: Assign y_pred_class to the X[test] (n_subj values)
 
-for t_iter, (train, test) in enumerate(sss.split(X, y)):
-    print('Iteration {}'.format(t_iter))
-    X_train, X_test = X[train], X[test]
-    y_train, y_test = y[train], y[test]
-    for clf_name, clf in classifiers.items():
-        # Fit the model on the training set
-        clf.fit(X_train, y_train)
-
-        # Predict the test set
-        y_pred_proba = clf.predict_proba(X_test)[:, 1]
-        y_pred_class = clf.predict(X_test)
-
-        auc_score = roc_auc_score(y_test, y_pred_proba)
-        prec_score = precision_score(y_test, y_pred_class)
-        rec_score = recall_score(y_test, y_pred_class)
-
-        results['Iteration'].append(t_iter)
-        results['Classifier'].append(clf_name)
-        results['AUC'].append(auc_score)
-        results['Precision'].append(prec_score)
-        results['Recall'].append(rec_score)
+            results[f_num, n_subj, t_iter] = np.round(np.mean(y_score))
 
 
 df = pd.DataFrame(results)
