3cbc6ae25ab92e742c19706281763a302f97b3d3
coma-meth <coma.meth@gmail.com>
2018-07-12 13:48:53 +0200

    compute features debug

--- ppln/compute_mask_atlas_meansLiege.py
+++ ppln/compute_mask_atlas_meansLiege.py
@@ -4,17 +4,21 @@ import pandas as pd
 from pypet.features import compute_regional_features
 
 if os.uname()[1] == 'antogeo-XPS':
-    db_path = '/home/antogeo/data/PET/pet_suv_db/Liege'
+    db_path = '/home/antogeo/data/PET/pet_suv_db/'
 elif os.uname()[1] == 'comameth':
-    db_path = '/home/coma_meth/Documents/PET/pet_suv_db/Liege'
+    db_path = '/home/coma_meth/Documents/PET/pet_suv_db/'
 elif os.uname()[1] in ['mia.local', 'mia']:
-    db_path = '/Users/fraimondo/data/pet_suv_db/Liege'
+    db_path = '/Users/fraimondo/data/pet_suv_db/'
 
 results = 'group_results_SUV'
+folders = ['Liege', 'Paris']
+all_df = []
+for folder in folders:
+    meta_fname = op.join(db_path, folder, 'extra', folder + '_meta.xls')
+    s_path = op.join(db_path, folder)
+    all_df = compute_regional_features(s_path, meta_fname, ftype='s8SUV')
+    print('{} {}'.format(s_path, meta_fname))
+    dframe = pd.DataFrame(all_df)
 
-meta_fname = op.join(db_path, 'extra', 'SUV_database10172017.xlsx')
-all_df = compute_regional_features(db_path, meta_fname)
-
-dframe = pd.DataFrame(all_df)
-
-dframe.to_csv(op.join(db_path, results, 'db_GM_masks_atlas.csv'))
+    dframe.to_csv(op.join(
+        db_path, folder, results, folder + '_db_GM_masks_atlas.csv'))
