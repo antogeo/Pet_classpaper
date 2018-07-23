0c41fc4c960567ac3ea81a82153ff43c3d362a1b
coma-meth <coma.meth@gmail.com>
2018-07-23 14:14:33 +0200

    add weight eval results

--- model_eval/weights_class.py
+++ model_eval/weights_class.py
@@ -111,7 +111,7 @@ classif = ['SVC_fs20p', 'SVC_fs10p', 'RF_w']
 fig, ax = plt.subplots(1, 1)
 paper_rc = {'lines.linewidth': .6, 'lines.markersize': 6}
 sns.set_context("paper", rc=paper_rc)
-
+df['Weight Val'] = df['Weight Val'].round(4)
 sns.pointplot(x="Weight Val", y="Recall", hue="Classifier", data=df, ax=ax,
                 hue_order=classif)
 sns.pointplot(x="Weight Val", y="Precision", hue="Classifier", data=df, ax=ax,
@@ -122,6 +122,6 @@ sns.pointplot(x="Weight Val", y="AUC", hue="Classifier", data=df, ax=ax,
 ax.legend(bbox_to_anchor=(1.01, 1), loc=2, borderaxespad=0.)
 ax.tick_params(axis='x', direction='out', length=3, width=1, grid_color='r',
                labelrotation=90, grid_alpha=0.5)
-ax.set_aspect('equal', axis='x', adjustable='datalim')
-ax.xaxis.set_major_locator(ticker.MultipleLocator(10))
-ax.xaxis.set_major_formatter(ticker.FormatStrFormatter('%2.2f'))
+# # ax.set_aspect('equal', axis='x', adjustable='datalim')
+# # ax.xaxis.set_major_locator(ticker.MultipleLocator(10))
+# ax.xaxis.set_major_formatter(ticker.FormatStrFormatter('%2.2f'))
