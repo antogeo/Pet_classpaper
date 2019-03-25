9a477176009b06f746d722b97ec084e7629f0bf7
antogeo <antogeo@gmail.com>
2019-03-25 13:20:40 +0100

    calc sim_mat manually & plot

--- features/compute_feat_matrix.py
+++ features/compute_feat_matrix.py
@@ -4,11 +4,10 @@ import os.path as op
 import numpy as np
 import pandas as pd
 from collections import OrderedDict
-from sklearn.model_selection import LeaveOneOut
+from sklearn.model_selection import StratifiedKFold
 from sklearn.linear_model import LogisticRegression
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import RobustScaler
-from scipy.spatial.distance import pdist, squareform
 import seaborn as sns
 import re
 
@@ -56,38 +55,55 @@ classifiers['lr'] = Pipeline([
         ('clf', LogisticRegression())
     ])
 
-results = dict()
-results['clf'] = []
-results['feature'] = []
-results['subj'] = []
-results['score'] = []
-results['proba'] = []
-results['lbl'] = []
-loo = LeaveOneOut()
-for train, test in loo.split(X):
-    for f_num, feature in enumerate(ord_markers):
-        X_train, X_test = X[train, f_num], X[test, f_num]
-        y_train, y_test = y[train], y[test]
-        y_score = []
-        for clf_name, clf in classifiers.items():
-            # Fit the model on the training set
-            clf.fit(X_train.reshape(-1, 1), y_train)
-            y_proba = clf.predict_proba(X_test.reshape(-1, 1))[:, 1]
-            y_score = clf.predict(X_test.reshape(-1, 1))
-            # TODO: Assign y_pred_class to the X[test] (n_subj values)
-            results['clf'].append(clf_name)
-            results['feature'].append(feature)
-            results['subj'].append(test[0])
-            results['score'].append(y_score[0])
-            results['proba'].append(y_proba[0])
-            results['lbl'].append(y[test][0])
+# results = dict()
+# results['repet'] = []
+# # results['clf'] = []
+# results['feature'] = []
+# results['subj'] = []
+# results['score'] = []
+# results['fold'] = []
+# # results['proba'] = []
+# results['lbl'] = []
+iters = 50
+subj_feat = np.zeros((iters, X.shape[1], X.shape[0])) + 2
 
-df_res = pd.DataFrame(results)
-mat = df_res.pivot(
-    values='score', index='feature', columns='subj')
-dis_mat = pdist(mat, metric='minkowski', p=1)
-sq_sim = pd.DataFrame((max(dis_mat) - squareform(dis_mat)),
-                      columns=ord_markers, index=ord_markers)
+for iter in range(iters):
+    folds = StratifiedKFold(n_splits=5, random_state=iter)
+    for train, test in folds.split(X, y):
+        for f_num, feature in enumerate(ord_markers):
+            X_train, X_test = X[train, f_num], X[test, f_num]
+            y_train, y_test = y[train], y[test]
+            for clf_name, clf in classifiers.items():
+                # Fit the model on the training set
+                clf.fit(X_train.reshape(-1, 1), y_train)
+                # y_proba = clf.predict_proba(X_test.reshape(-1, 1))[:, 1]
+                y_score = clf.predict(X_test.reshape(-1, 1))
+                for test_num, test_ind in enumerate(test):
+                    print(test_num)
+                    subj_feat[iter, f_num, test_ind] = y_score[test_num]
+                # # results['clf'].append(clf_name)
+                # results['feature'].append(feature)
+                # results['subj'].append(test)
+                # results['score'].append(y_score)
+                # # results['proba'].append(y_proba[0])
+                # results['lbl'].append(y[test][0])
+subj_feat2d = np.sum(subj_feat, axis=0)
+normed_feat = (subj_feat2d - subj_feat2d.min()) / (
+               subj_feat2d.max() - subj_feat2d.min())
 
-sns.heatmap(sq_sim)
-df.to_csv('../group_results_SUV/feat_similarity_mat.csv')
+df_res = pd.DataFrame(subj_feat2d, index=ord_markers,
+                      columns=np.array(range(1, X.shape[0] + 1)))
+
+sim_mat = np.zeros((X.shape[1], X.shape[1]))
+
+for i in range(X.shape[1]):
+    for j in range(i, X.shape[1]):
+        # print(i, j)
+        for x in range(X.shape[0]):
+            sim_mat[i, j] = sim_mat[i, j] + np.abs(
+                1 - normed_feat[i, x] - normed_feat[j, x])
+        sim_mat[j, i] = sim_mat[i, j]
+
+df_sim = pd.DataFrame(sim_mat, index=ord_markers, columns=ord_markers)
+sns.heatmap(df_sim)
+df_sim.to_csv('../group_results_SUV/feat_similarity_mat.csv')
