4074b338a680678e00ffaf41800a0ac32aaff868
antogeo <antogeo@gmail.com>
2019-03-27 22:52:43 +0100

    plot weights eval

--- ppln/plot_series.py
+++ ppln/plot_series.py
@@ -1,11 +1,12 @@
-import os
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+import numpy as np
 
-df = pd.read_csv('group_results_SUV/Liege_weight_eval.csv')
+df = pd.read_csv('./group_results_SUV/weights_eval_20190326.csv')
 
-# fig, axes = plt.subplots(1, 3, figsize=(12, 6))
+# fig, axes = plt.subplots(3, 1, figsize=(12, 6))
 # sns.swarmplot(
 #     x="Classifier", y="Recall", hue="Classifier", data=df, ax=axes[0])
 # sns.swarmplot(
@@ -15,17 +16,46 @@ df = pd.read_csv('group_results_SUV/Liege_weight_eval.csv')
 # # plt.savefig('figures/model_compar.pdf')
 # plt.show()
 
-fig, axes = plt.subplots(1, 3, figsize=(12, 6))
-sns.lineplot(
-    x="Depth", y="Recall", hue="Classifier", data=df, ax=axes[0])
-sns.lineplot(
-    x="Depth", y="Precision", hue="Classifier", data=df, ax=axes[1])
-sns.lineplot(
-    x="Depth", y="AUC", hue="Classifier", data=df, ax=axes[2])
+res = dict()
+res['weights'] = []
+res['mean rec'] = []
+res['mean pre'] = []
+for weight in np.unique(df['Weight Val']):
+    res['weights'].append(weight)
+    res['mean rec'].append(np.mean(df.loc[(df['Classifier'] == 'SVC_fs10p') & (
+        df['Weight Val'] == weight)]['Recall']))
+    res['mean pre'].append(np.mean(df.loc[(df['Classifier'] == 'SVC_fs10p') & (
+        df['Weight Val'] == weight)]['Precision']))
+results = pd.DataFrame(res)
 
-axes[0].set_ylim(0,)
-axes[1].set_ylim(0,)
-axes[2].set_ylim(0,)
+line1 = results.loc[np.abs(results['mean rec'] - .95).idxmin(), 'weights']
+line2 = results.loc[np.abs(results['mean pre'] - .75).idxmin(), 'weights']
 
+fig, axes = plt.subplots(3, 1, figsize=(12, 6))
+
+sns.lineplot(
+    x="Weight Val", y="Recall", hue="Classifier", data=df[df[
+        'Classifier'] != 'SVC_fs20p'], ax=axes[0])
+plt.xlabel('MCS/UWS (Weights Ratio)')
+
+axes[0].axvline(line1, color='red')
+
+sns.lineplot(
+    x="Weight Val", y="Precision", hue="Classifier", data=df[df[
+        'Classifier'] != 'SVC_fs20p'], ax=axes[1])
+
+axes[1].axvline(line2, color='red')
+
+sns.lineplot(
+    x="Weight Val", y="AUC", hue="Classifier", data=df[df[
+        'Classifier'] != 'SVC_fs20p'], ax=axes[2])
+
+axes[0].set_xlabel('MCS/UWS (Weights Ratio)')
+axes[1].set_xlabel('MCS/UWS (Weights Ratio)')
+axes[2].set_xlabel('MCS/UWS (Weights Ratio)')
+
+axes[0].xaxis.set_major_locator(ticker.MultipleLocator(.2))
+axes[1].xaxis.set_major_locator(ticker.MultipleLocator(.2))
+axes[2].xaxis.set_major_locator(ticker.MultipleLocator(.2))
 # plt.savefig('figures/model_compar_box.pdf')
 plt.show()
