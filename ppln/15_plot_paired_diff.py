31a4eba290bde3df2a27bc592f7e0d0c2fe2eb91
antogeo <antogeo@gmail.com>
2019-05-01 23:05:58 +0200

    add paired compar plots

--- ppln/15_plot_paired_diff.py
+++ ppln/15_plot_paired_diff.py
@@ -13,6 +13,18 @@ df = df.reset_index()
 contr_df = pd.read_csv('group_results_SUV/Clfs_contrast_nAAL.csv')
 diff_df = contr_df.loc[contr_df['Contrast'].str.contains("-Dummy")]
 
+rf_auc = df[df['Classifier'] == 'RF']['AUC'].mean()
+rf_rec = df[df['Classifier'] == 'RF']['Recall'].mean()
+rf_prec = df[df['Classifier'] == 'RF']['Precision'].mean()
+svc_rec_auc = df[df['Classifier'] == 'SVC_rec']['AUC'].mean()
+svc_rec_rec = df[df['Classifier'] == 'SVC_rec']['Recall'].mean()
+svc_rec_prec = df[df['Classifier'] == 'SVC_rec']['Precision'].mean()
+svc_prec_auc = df[df['Classifier'] == 'SVC_prec']['AUC'].mean()
+svc_prec_rec = df[df['Classifier'] == 'SVC_prec']['Recall'].mean()
+svc_prec_prec = df[df['Classifier'] == 'SVC_prec']['Precision'].mean()
+Dum_auc = df[df['Classifier'] == 'Dummy']['AUC'].mean()
+Dum_rec = df[df['Classifier'] == 'Dummy']['Recall'].mean()
+Dum_prec = df[df['Classifier'] == 'Dummy']['Precision'].mean()
 
 
 plt.figure(1)
@@ -26,15 +38,29 @@ pypet.viz.plot_values(df, values='AUC',
                       style='violinplot')
 sum((diff_df[diff_df['Contrast'] == 'RF-Dummy']['AUC'] < 0) * 1)
 
+yposlist = df.groupby(['Classifier']).mean()
+xposlist = range(len(yposlist))
+stringlist = [rf_auc, svc_prec_auc, svc_rec_auc, Dum_auc]
+for i in range(len(stringlist)):
+    ax1.text(xposlist[i] + 0.05,
+             stringlist[i], format(round(stringlist[i], 2)))
+
+
 ax2 = plt.subplot(gs[1])
 
 sns.boxplot(data=diff_df, x='AUC', y='Contrast', ax=ax2)
 print('{0} times that SVC_rec-Dummy AUC was below 0'.format(
-    sum((diff_df[diff_df['Contrast'] == 'SVC_rec-Dummy']['AUC'] < 0) * 1)))
+    sum((diff_df[diff_df['Contrast'] == 'SVC_rec-Dummy'][
+        'AUC'] < 0) * 1)))
 print('{0} times that SVC_prec-Dummy AUC was below 0'.format(
-    sum((diff_df[diff_df['Contrast'] == 'SVC_prec-Dummy']['AUC'] < 0) * 1)))
+    sum((diff_df[diff_df['Contrast'] == 'SVC_prec-Dummy'][
+        'AUC'] < 0) * 1)))
 print('{0} times that RF-Dummy AUC was below 0 '.format(
-    sum((diff_df[diff_df['Contrast'] == 'RF-Dummy']['AUC'] < 0) * 1)))
+    sum((diff_df[diff_df['Contrast'] == 'RF-Dummy'][
+        'AUC'] < 0) * 1)))
+
+ax2.axvline(0, linestyle='--', color='red', alpha=0.4)
+
 plt.figure(2)
 gs2 = gridspec.GridSpec(2, 1, height_ratios=[4, 1])
 
@@ -45,15 +71,25 @@ pypet.viz.plot_values(df, values='Recall',
                       classes=['RF', 'SVC_prec', 'SVC_rec', 'Dummy'],
                       style='violinplot')
 
+yposlist = df.groupby(['Classifier']).mean()
+xposlist = range(len(yposlist))
+stringlist = [rf_rec, svc_prec_rec, svc_rec_rec, Dum_rec]
+for i in range(len(stringlist)):
+    ax21.text(xposlist[i] + 0.05,
+              stringlist[i], format(round(stringlist[i], 2)))
+
 ax22 = plt.subplot(gs2[1])
 sns.boxplot(data=diff_df, x='Recall', y='Contrast', ax=ax22)
-
+ax22.axvline(0, linestyle='--', color='red', alpha=0.4)
 print('{0} times that SVC_rec-Dummy Recall was below 0'.format(
-    sum((diff_df[diff_df['Contrast'] == 'SVC_rec-Dummy']['Recall'] < 0) * 1)))
+    sum((diff_df[diff_df['Contrast'] == 'SVC_rec-Dummy'][
+        'Recall'] < 0) * 1)))
 print('{0} times that SVC_prec-Dummy Recall was below 0'.format(
-    sum((diff_df[diff_df['Contrast'] == 'SVC_prec-Dummy']['Recall'] < 0) * 1)))
+    sum((diff_df[diff_df['Contrast'] == 'SVC_prec-Dummy'][
+        'Recall'] < 0) * 1)))
 print('{0} times that RF-Dummy Recall was below 0 '.format(
-    sum((diff_df[diff_df['Contrast'] == 'RF-Dummy']['Recall'] < 0) * 1)))
+    sum((diff_df[diff_df['Contrast'] == 'RF-Dummy'][
+        'Recall'] < 0) * 1)))
 
 plt.figure(3)
 gs3 = gridspec.GridSpec(2, 1, height_ratios=[4, 1])
@@ -64,14 +100,29 @@ pypet.viz.plot_values(df, values='Precision',
                       target='Classifier', axes=ax31,
                       classes=['RF', 'SVC_prec', 'SVC_rec', 'Dummy'],
                       style='violinplot')
-
+yposlist = df.groupby(['Classifier']).mean()
+xposlist = range(len(yposlist))
+stringlist = [rf_prec, svc_prec_prec, svc_rec_prec, Dum_prec]
+for i in range(len(stringlist)):
+    ax31.text(xposlist[i] + 0.05,
+              stringlist[i], format(round(stringlist[i], 2)))
 ax32 = plt.subplot(gs3[1])
 sns.boxplot(data=diff_df, x='Precision', y='Contrast', ax=ax32)
+ax32.axvline(0, linestyle='--', color='red', alpha=0.4)
 
+xposlist = df.groupby(['Classifier']).mean()
+xposlist = range(len(yposlist))
+stringlist = [rf_prec, svc_prec_prec, svc_rec_prec, Dum_prec]
+for i in range(len(stringlist)):
+    ax31.text(xposlist[i] + 0.05,
+              stringlist[i], format(round(stringlist[i], 2)))
 print('{0} times that SVC_rec-Dummy Precision was below 0'.format(
-    sum((diff_df[diff_df['Contrast'] == 'SVC_rec-Dummy']['Precision'] < 0) * 1)))
+    sum((diff_df[diff_df['Contrast'] == 'SVC_rec-Dummy'][
+        'Precision'] < 0) * 1)))
 print('{0} times that SVC_prec-Dummy Precision was below 0'.format(
-    sum((diff_df[diff_df['Contrast'] == 'SVC_prec-Dummy']['Precision'] < 0) * 1)))
+    sum((diff_df[diff_df['Contrast'] == 'SVC_prec-Dummy'][
+        'Precision'] < 0) * 1)))
 print('{0} times that RF-Dummy Precision was below 0 '.format(
-    sum((diff_df[diff_df['Contrast'] == 'RF-Dummy']['Precision'] < 0) * 1)))
+    sum((diff_df[diff_df['Contrast'] == 'RF-Dummy'][
+        'Precision'] < 0) * 1)))
 plt.show()
